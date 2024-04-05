cc89e8768da82375812831ef1e800f543b2f03ae
Mark Jayson Doma <markjaysondoma@Marks-MacBook-Air.local>
2024-04-05 14:59:49 +0800

    add members page

--- pages/members.js
+++ pages/members.js
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
-import Members from "@/components/Members";
+import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/20/solid";
+
+import axios from "axios";
+
+//Context
+import EventContext from "@/context/eventContext";
 
 import { Fragment, useState } from "react";
 import { Dialog, Menu, Transition } from "@headlessui/react";
@@ -21,8 +28,183 @@ import {
   MagnifyingGlassIcon,
 } from "@heroicons/react/20/solid";
 
-export default function Sample() {
+// const calEvents = [
+//   { date: '2023-12-02', eventName: 'Open Door' },
+//   { date: '2023-12-15', eventName: 'Event 2' },
+//   { date: '2023-12-20', eventName: 'Event 3' },
+// ];
+
+const people = [
+  {
+    name: "Jane Cooper",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Jane Cooper",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Jane Cooper",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Jane Cooper",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Jane Cooper",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Jane Cooper",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Jane Cooper",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Jane Cooper",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+];
+
+export default function Members() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [eventsOptions, setEventsOptions] = useState([]);
+
+  // const handleScan = (data) => {
+  //   console.log("Scanned QR Code:", data);
+  //   // Add your logic to handle the scanned QR code data here
+  // };
+
+  const [scanResult, setScanResult] = useState("");
+
+  const handleScan = (data) => {
+    setScanResult(data);
+    // console.log("Scanned QR Code:", data);
+  };
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get(
+          `https://www.googleapis.com/calendar/v3/calendars/ligayasdg@gmail.com/events/`,
+          {
+            params: {
+              // key: 'AIzaSyC0OBwnEO2n244bIYqjhvTkdo1_QaZIjtY',
+              key: "AIzaSyAbX2qOg-8MGiK2HHxpNT0DAwCogdHpJJM",
+            },
+          }
+        );
+
+        // Filter events that start with "SDG" or "Open"
+        const filteredEvents = response.data.items.filter((item) => {
+          if (item.status === "cancelled") {
+            // Exclude cancelled events
+            return false;
+          }
+          const summary = item.summary.toLowerCase();
+          return (
+            summary.startsWith("sdg: district") ||
+            summary.startsWith("open") ||
+            summary.startsWith("bid") ||
+            summary.startsWith("choices") ||
+            summary.startsWith("plt")
+          );
+        });
+
+        // Filter events with dates between 07/30/23 and today's date
+        const currentDate = new Date();
+        const events = filteredEvents.map((item, index) => {
+          if (item.status === "cancelled") {
+            // Exclude cancelled events
+            return false;
+          }
+          let value;
+          if (item.start.dateTime) {
+            value = new Date(item.start.dateTime).toLocaleDateString("en-US");
+          } else {
+            value = new Date(item.start.date).toLocaleDateString("en-US");
+          }
+
+          // Parse the date string and compare it with the currentDate
+          const eventDate = new Date(value);
+          if (eventDate >= new Date("2023-07-29") && eventDate <= currentDate) {
+            return {
+              value,
+              label: item.summary,
+              key: `${value}-${index}`,
+            };
+          }
+          return null;
+        });
+
+        // Remove null values from the events array (events that didn't meet the criteria)
+        const filteredEventsOptions = events.filter(Boolean);
+
+        setEventsOptions(filteredEventsOptions);
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  const eventOptions = eventsOptions;
+
+  // const eventOptions = [
+  //   { value: '2023-07-30', label: 'July District Gathering' },
+  //   { value: '2023-03-26', label: 'March District Gathering' },
+  //   { value: '2023-08-06', label: 'August Open Door' },
+  //   { value: '2023-08-04', label: 'Test Event' },
+  // ];
+
+  // const [selectedEvent, setSelectedEvent] = useState(eventsOptions[0]);
+  const [selectedEvent, setSelectedEvent] = useState(eventOptions[0]);
+
+  console.log(selectedEvent);
 
   return (
     <>
@@ -114,9 +296,66 @@ export default function Sample() {
 
           {/* Main section */}
           <main className="py-10">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <Members />
-            </div>
+            <ul
+              role="list"
+              className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+            >
+              {people.map((person) => (
+                <li
+                  key={person.email}
+                  className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
+                >
+                  <div className="flex flex-1 flex-col p-8">
+                    <img
+                      className="mx-auto h-32 w-32 flex-shrink-0 rounded-full"
+                      src={person.imageUrl}
+                      alt=""
+                    />
+                    <h3 className="mt-6 text-sm font-medium text-gray-900">
+                      {person.name}
+                    </h3>
+                    <dl className="mt-1 flex flex-grow flex-col justify-between">
+                      <dt className="sr-only">Title</dt>
+                      <dd className="text-sm text-gray-500">{person.title}</dd>
+                      <dt className="sr-only">Role</dt>
+                      <dd className="mt-3">
+                        <span className="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                          {person.role}
+                        </span>
+                      </dd>
+                    </dl>
+                  </div>
+                  <div>
+                    <div className="-mt-px flex divide-x divide-gray-200">
+                      <div className="flex w-0 flex-1">
+                        <a
+                          href={`mailto:${person.email}`}
+                          className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                        >
+                          <EnvelopeIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                          Email
+                        </a>
+                      </div>
+                      <div className="-ml-px flex w-0 flex-1">
+                        <a
+                          href={`tel:${person.telephone}`}
+                          className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                        >
+                          <PhoneIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                          Call
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </main>
         </div>
       </div>
