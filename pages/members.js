2da4e0a399aaf67a06da66619d2d59f82c798f89
Mark Doma <markdoma10@gmail.com>
2024-04-07 21:51:18 +0800

    adjust data

--- pages/members.js
+++ pages/members.js
@@ -40,7 +40,7 @@ const people = [
     service: "Music Ministry",
     status: "Covenanted",
     email: "janecooper@example.com",
-    bday: "04/10/1978",
+    telephone: "+1-202-555-0170",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
