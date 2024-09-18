463ebb17ab8e99b881e242e059b966205b5b14a4
Mark Doma <markdoma10@gmail.com>
2024-09-18 10:35:19 +0800

    add call and message logic to buttons

--- components/Members/Details.js
+++ components/Members/Details.js
@@ -65,10 +65,10 @@ export default function Details({ member }) {
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium leading-6 text-gray-900">
-              Pastoral Leader
+              Contact Number
             </dt>
             <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {member.pl}
+              {member.contact}
             </dd>
           </div>
         </dl>
