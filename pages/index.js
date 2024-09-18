69dede4f0cbdfd2d5444dd1f15d905b40ed29fc7
Mark Doma <markdoma10@gmail.com>
2024-09-18 22:30:33 +0800

    change tab design

--- pages/index.js
+++ pages/index.js
@@ -58,12 +58,12 @@ export default function Ligayasdg() {
             ))}
           </div> */}
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a
+            {/* <a
               href="/login"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
               Log in <span aria-hidden="true">&rarr;</span>
-            </a>
+            </a> */}
           </div>
         </nav>
         <Dialog
@@ -107,12 +107,12 @@ export default function Ligayasdg() {
                   ))}
                 </div> */}
                 <div className="py-6">
-                  <a
+                  {/* <a
                     href="/login"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Log in
-                  </a>
+                  </a> */}
                 </div>
               </div>
             </div>
