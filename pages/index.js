706fc41b7bc706e355dfe52ca616781707d96b57
Mark Doma <markdoma10@gmail.com>
2025-01-16 09:49:17 +0800

    add generic homepage for members that are not PL

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { useAuth } from "../context/AuthContext";
 import Loading from "../components/Misc/Loading";
 import { db } from "@/utils/firebase";
 import Home from "@/components/Members/Home";
+import Member from "@/components/Members/Member"; // Import Member component
 import {
   getDocs,
   query,
@@ -32,22 +33,28 @@ export default function Ligayasdg() {
     navigation,
     initialMembers,
     loading, // Use loading from context
+    userDetails, // Add userDetails from context
   } = useAuth();
 
   console.log(error);
 
-  // Conditional rendering: If user is authenticated and email is found in master_data, show the Home component
+  // Conditional rendering: If user is authenticated and email is found in master_data, show the appropriate component
   if (user && initialMembers.length > 0) {
-    return (
-      <Home
-        initialMembers={initialMembers}
-        userEmail={user.email}
-        userRole={user.role}
-        plName={user.pl_name}
-        notRegistered={notRegistered}
-        setNotRegistered={setNotRegistered}
-      />
-    );
+    if (userDetails.service_role === "pl") {
+      // Use service_role from userDetails
+      return (
+        <Home
+          initialMembers={initialMembers}
+          userEmail={user.email}
+          userRole={user.role}
+          plName={user.pl_name}
+          notRegistered={notRegistered}
+          setNotRegistered={setNotRegistered}
+        />
+      );
+    } else {
+      return <Member />;
+    }
   }
   return null;
 }
